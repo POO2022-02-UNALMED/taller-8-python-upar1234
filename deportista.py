a60798831872bf978ea6462563c556afd9f507cb
upar1234 <upar1234@gmail.com>
2022-10-18 14:01:00 -0500

    s

--- deportista.py
+++ deportista.py
@@ -5,9 +5,7 @@ class Deportista(Persona):
     def __init__(self,nombre,edad,altura,sexo,deporte,años):
         super().__init__(nombre,edad,altura,sexo)
         self._deporte=deporte
-        self._añosPracticando=años
-
-    
+        self._añosPracticando=años   
     def getDeporte(self):
         return self._deporte
     
